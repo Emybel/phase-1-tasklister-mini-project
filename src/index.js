8b057c2326dada7046b1dab44a1312a4d1753caa
Imane Belaid <belaidimane@gmail.com>
2023-08-18 19:38:41 +0100

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addToDo(e.target.task.value);
+    form.reset();
+  })
 });
+
+function addToDo(todo){
+
+  //* Create the list items with the delate button
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+
+  //* Add EventLinstener to delete button with handleDelete callback function
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = "x";
+  li.textContent = ` ${ todo} ` ;
+  li.appendChild(btn)
+  document.querySelector("#tasks").appendChild(li);
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
